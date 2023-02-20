d5ff75b8fbbf1c363c805609c05bb98029906121
Amtu Polycarp Dawum <amtujnr@yahoo.com>
2023-02-20 08:48:44 -0800

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
