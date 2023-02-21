d04b80f72d975cce450bb1d868b80d3a43e529ef
Amtu Polycarp Dawum <amtujnr@yahoo.com>
2023-02-20 18:24:21 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,10 @@ int b;
 long int c;
 long long int d;
 float f;
- {
-printf("Size of a char:%lu byte(s)\n", (unsigned long)size of (a));
- }
-printf("Size of a int :%lu byte(s)\n", (unsigned long)size of (b));
-printf("Size of a long int:%lu byte(s)\n", (unsigned long)size of (c));
-printf("Size of a long long int:%lu byte(s)\n", (unsigned long)size of (d));
-printf("Size of float:%lu byte(s)\n", (unsigned long)size of (f));
+printf("Size of a char:%lu byte(s)\n", sizeof(char));
+printf("Size of a int :%lu byte(s)\n", sizeof(int));
+printf("Size of a long int:%lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int:%lu byte(s)\n", sizeof(long long int));
+printf("Size of float:%lu byte(s)\n", sizeof(float));
 return (0);
 }
