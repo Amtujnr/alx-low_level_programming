b692953dc11aa811bced2fb2ed7170c985a1ed8c
Amtu Polycarp Dawum <amtujnr@yahoo.com>
2023-02-22 13:56:21 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 0(success)
+ * on error, -1 is returned
+ */
+int _putchar(char c)
+{
+return (0);
+}
